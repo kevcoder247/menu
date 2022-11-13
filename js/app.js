046204530636fcfd6049daa65529ec38b155eec1
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-11-13 16:11:09 -0500

    wip: menu items filtered when corresponding button is clicked

--- js/app.js
+++ js/app.js
@@ -76,22 +76,45 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 
+const filterBtns = document.querySelectorAll('.filter-btn');
+//Load Items
 window.addEventListener('DOMContentLoaded', function(){
-  let displayMenu = menu.map(function(item) {
-  //  console.log(item);
+  displayMenuItems(menu);
+});
 
-
-   return ` <article class="menu-item">
-   <img src=${item.img} class="photo" alt=${item.title}>
-   <div class="item-info">
-     <header>
-       <h4>${item.title}</h4>
-       <h4 class="price">$${item.price}</h4>
-     </header>
-      <p class="item-text">${item.desc}</p>
-   </div>
- </article>`
-  })
-  displayMenu = displayMenu.join('');
-  sectionCenter.innerHTML = displayMenu;
+//filter items
+filterBtns.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function(menuItem){
+    if(menuItem.category === category){
+      return menuItem;
+    }
+  });
+    if(category === "all"){
+      displayMenuItems(menu)
+    }else{
+      displayMenuItems(menuCategory);
+    }
+  });
 })
+
+
+
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item) {
+    return ` <article class="menu-item">
+     <img src=${item.img} class="photo" alt=${item.title}>
+     <div class="item-info">
+       <header>
+         <h4>${item.title}</h4>
+         <h4 class="price">$${item.price}</h4>
+       </header>
+        <p class="item-text">${item.desc}</p>
+     </div>
+   </article>`
+    })
+    displayMenu = displayMenu.join('');
+    sectionCenter.innerHTML = displayMenu;
+}
