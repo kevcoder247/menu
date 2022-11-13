25d046f6f615fbc47af2d13e3b0e551675a9e930
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-11-13 16:38:47 -0500

    code refactor refactoring

--- js/app.js
+++ js/app.js
@@ -76,30 +76,14 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 
-const filterBtns = document.querySelectorAll('.filter-btn');
+const container = document.querySelector('.btn-container')
+
+
 //Load Items
 window.addEventListener('DOMContentLoaded', function(){
   displayMenuItems(menu);
+  displayMenuButtons();
 });
-
-//filter items
-filterBtns.forEach(function(btn){
-  btn.addEventListener('click', function(e){
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function(menuItem){
-    if(menuItem.category === category){
-      return menuItem;
-    }
-  });
-    if(category === "all"){
-      displayMenuItems(menu)
-    }else{
-      displayMenuItems(menuCategory);
-    }
-  });
-})
-
-
 
 
 function displayMenuItems(menuItems){
@@ -117,4 +101,39 @@ function displayMenuItems(menuItems){
     })
     displayMenu = displayMenu.join('');
     sectionCenter.innerHTML = displayMenu;
+}
+
+
+
+
+
+function displayMenuButtons(){
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    }
+    return values
+  }, ['all'])
+  const categoryBtns = categories.map(function(category){
+    return ` <button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+  }).join('')
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll('.filter-btn');
+
+//filter items
+filterBtns.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function(menuItem){
+    if(menuItem.category === category){
+      return menuItem;
+    }
+  });
+    if(category === "all"){
+      displayMenuItems(menu)
+    }else{
+      displayMenuItems(menuCategory);
+    }
+  });
+});  
 }
